20089a87e83b9486ca5c9b975c0b971242415dac
Kim Wilson <k.wilson541@gmail.com>
2017-01-08 17:45:03 +0000

    Article List View can display multiple articles, test passing

--- spec/article-list-view-spec.js
+++ spec/article-list-view-spec.js
@@ -8,7 +8,17 @@ articleList.showArticles = function() {
 	return [article]
 };
 
+multipleArticleList = {}
+multipleArticleList.showArticles = function() {
+	return [article, article]
+};
+
 (function testListArticles() {
 	var articleListView = new ArticleListView(articleList);
 	assert.isTrue(articleListView.listArticles() === '<ul><li><div>Headline</div></li></ul>')
+})();
+
+(function testListMultipleArticles() {
+	var articleListView = new ArticleListView(multipleArticleList);
+	assert.isTrue(articleListView.listArticles() === '<ul><li><div>Headline</div></li><li><div>Headline</div></li></ul>')
 })();
