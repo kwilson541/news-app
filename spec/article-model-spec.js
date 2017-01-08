d8d764d422b496ac64cde1d5362e4189adec81f0
Kim Wilson <k.wilson541@gmail.com>
2017-01-08 14:14:55 +0000

    Article model now includes body property, test passing

--- spec/article-model-spec.js
+++ spec/article-model-spec.js
@@ -2,3 +2,8 @@
 	var article = new Article("Headline here");
 	assert.isTrue(article.returnHeadline() === "Headline here");
 })();
+
+(function testArticleCreatedWithBody() {
+	var article = new Article("Headline here", "Body here");
+	assert.isTrue(article.returnBody() === "Body here");
+})();
--- src/models/article-model.js
+++ src/models/article-model.js
@@ -1,11 +1,16 @@
 (function(exports) {
 
-	function Article(headline){
-		this._headline = headline
+	function Article(headline, body){
+		this._headline = headline;
+		this._body = body;
 	}
 
 	Article.prototype.returnHeadline = function() {
 		return this._headline;
+	};
+
+	Article.prototype.returnBody = function() {
+		return this._body;
 	};
 
 	exports.Article = Article;
